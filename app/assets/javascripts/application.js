a8d76f3172f662b8cf0b8c3fd531a17b970ed033
inv-ftw <inv.ftw@gmail.com>
2016-01-17 07:20:30 +0200

    lp features

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,4 +19,30 @@ $(document).ready(function () {
   setTimeout(function () {
     $('.preloader').fadeOut();
   }, 1000);
+
+  function slideImage(el) {
+    var nextId = $(el).data('id');
+    var $activeLi = $('ul.points li.active');
+    var currentId = $activeLi.data('id');
+    $activeLi.removeClass('active');
+    $(el).addClass('active');
+    $('img#'+currentId).animate({marginLeft: '-244px'}, function () {
+      $('img#'+currentId).hide();
+      $('img#'+nextId).css('margin-left', 0).fadeIn();
+    });
+  }
+
+  $('ul.points li').click(function (e) {
+    e.preventDefault();
+    slideImage(this);
+  });
+
+  var interval = setInterval(function () {
+    var currentId = parseInt($('ul.points li.active').data('id'));
+    currentId++;
+    if (currentId > 4) currentId = 1;
+    var $nextLi = $('ul.points li[data-id='+currentId+']');
+    slideImage($nextLi);
+  }, 5000);
+
 });
